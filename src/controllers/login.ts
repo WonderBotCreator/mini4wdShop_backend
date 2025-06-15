e8c3ab9e69bd169087ea28ae88b0442be4f4f621
WonderBotCreator <nonthanunnd@gmail.com>
2025-06-15 12:35:56 +0700

    modify login function

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,44 +1,44 @@
-// import jwt from "jsonwebtoken";
-// import bcrypt from "bcrypt";
-// import { PrismaClient } from '@prisma/client';
-// import { withAccelerate } from '@prisma/extension-accelerate';
-// import express, { NextFunction } from 'express';
-// import z from "zod";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import express, { NextFunction } from 'express';
+import z from "zod";
 
-// const prisma = new PrismaClient()
-//     .$extends(withAccelerate());
+const prisma = new PrismaClient()
+    .$extends(withAccelerate());
 
-// const loginRouter = express.Router()
+const loginRouter = express.Router()
 
 
-// loginRouter.post('/', async (req, res) => {
-//     const { email, password } = req.body;
-//     const user = await prisma.user.findUnique({
-//         where: {
-//             email: email,
-//         },
-//     })
+loginRouter.post('/', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await prisma.customer.findUnique({
+        where: {
+            email: email,
+        },
+    })
 
-//     console.log(user)
+    console.log(user)
 
-//     const passwordCorrect = user === null
-//         ? false
-//         : await bcrypt.compare(password, user.passwordHash)
+    const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(password, user.passwordHash)
 
-//     if (!(user && passwordCorrect)) {
-//         res.status(401).json({
-//             error: 'invalid username or password'
-//         })
-//     }
+    if (!(user && passwordCorrect)) {
+        res.status(401).json({
+            error: 'invalid username or password'
+        })
+    }
 
-//     const userForToken = {
-//         username: user?.username,
-//         id: user?.id
-//     }
+    const userForToken = {
+        username: user?.username,
+        id: user?.id
+    }
 
-//     const token = jwt.sign(userForToken, process.env.SECRET as string, {expiresIn: 60*60})
+    const token = jwt.sign(userForToken, process.env.SECRET as string, {expiresIn: 60*60})
 
-//     res.status(200).send({token, username: user?.username})
-// })
+    res.status(200).send({token, username: user?.username})
+})
 
-// export default loginRouter;
+export default loginRouter;
