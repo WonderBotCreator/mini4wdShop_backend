3dd1f859b10d3d6d0a2a76026ac67f99e3e8eb4e
WonderBotCreator <nonthanunnd@gmail.com>
2025-06-16 21:26:15 +0700

    edit post add item

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -1,19 +1,19 @@
 import * as jwt from 'jsonwebtoken';
 import bcrypt from "bcrypt";
-import { $Enums,PrismaClient } from '@prisma/client';
+import { $Enums, PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import express, { NextFunction, response } from 'express';
 import z from "zod";
 import { AccessoryType, Mini4wdType, NewProduct } from '../../types';
 
 
-const getTokenFrom = (auth:string|undefined): string|null=> {
-    
-    if(auth && auth.startsWith('Bearer ')){
-        console.log(auth.replace('Bearer ',''))
-        return auth.replace('Bearer ','')
+const getTokenFrom = (auth: string | undefined): string | null => {
+
+    if (auth && auth.startsWith('Bearer ')) {
+        console.log(auth.replace('Bearer ', ''))
+        return auth.replace('Bearer ', '')
     }
-   return null;
+    return null;
 }
 
 declare module 'jsonwebtoken' {
@@ -38,93 +38,87 @@ const prisma = new PrismaClient()
 
 const cartRouter = express.Router()
 
-cartRouter.get('/', async(request, response)=>{
-     
+cartRouter.get('/', async (request, response) => {
+
     const token = getTokenFrom(request.get('authorization'))
     //let decodedToken: DecodedToken|null = null;
 
     let userID = undefined
-    if(token!== null)
-    {
-         //decodedToken = jwt.verify(token, process.env.SECRET as string) 
-         userID = userIdFromJWT(token)
+    if (token !== null) {
+        //decodedToken = jwt.verify(token, process.env.SECRET as string) 
+        userID = userIdFromJWT(token)
     }
-   
-    if(userID === undefined)
-    {
-        response.status(400).send({message: "Error add book"})
+
+    if (userID === undefined) {
+        response.status(400).send({ message: "Error add book" })
         return
     }
-    
+
     const user = await prisma.customer.findUnique({
-        where:{
+        where: {
             id: userID
         },
-        include:{
+        include: {
             cart: true
         }
     })
 
     let products = []
 
-     const cart = await prisma.cart.findFirst({
-      where:{
-        owner:{
-          id: user?.id
+    const cart = await prisma.cart.findFirst({
+        where: {
+            owner: {
+                id: user?.id
+            }
+        },
+        include: {
+            cartItems: true
         }
-      },
-      include:{
-        cartItems: true
-      }
     })
 
-    if(cart?.cartItems.length)
-    {
-        const itemLength:number|0 =  cart?.cartItems.length
+    if (cart?.cartItems.length) {
+        const itemLength: number | 0 = cart?.cartItems.length
 
-        for(let i = 0;i< itemLength;i++)
-        {
+        for (let i = 0; i < itemLength; i++) {
             const product = await prisma.product.findUnique({
-                where:{
+                where: {
                     id: cart?.cartItems[i].productId
                 }
             })
 
             //console.log(product)
 
-            products.push({itemId: cart.cartItems[i].id, product: product, amount: cart?.cartItems[i].amount})
+            products.push({ itemId: cart.cartItems[i].id, product: product, amount: cart?.cartItems[i].amount })
         }
     }
-    
-    response.status(200).send({products: products, message: "get cart successfully", status: "success"})
+
+    response.status(200).send({ products: products, message: "get cart successfully", status: "success" })
 
 
 })
 
 
-cartRouter.delete('/:id', async(request, response)=>{
+cartRouter.delete('/:id', async (request, response) => {
     const itemId = request.params.id
     const token = getTokenFrom(request.get('authorization'))
     //let decodedToken: DecodedToken|null = null;
 
     let userID = undefined
-    if(token!== null)
-    {
-         //decodedToken = jwt.verify(token, process.env.SECRET as string) 
-         userID = userIdFromJWT(token)
+    if (token !== null) {
+        //decodedToken = jwt.verify(token, process.env.SECRET as string) 
+        userID = userIdFromJWT(token)
     }
-   
-    if(userID === undefined)
-    {
-        response.status(400).send({message: "Error cannot find any user", status: "error"})
+
+    if (userID === undefined) {
+        response.status(400).send({ message: "Error cannot find any user", status: "error" })
         return
     }
-    
+
     const user = await prisma.customer.findUnique({
-        where:{
+        where: {
             id: userID
         },
-        include:{
+        include: {
             cart: true
         }
     })
@@ -133,53 +127,50 @@ cartRouter.delete('/:id', async(request, response)=>{
 
 
     const deleteCartItem = await prisma.cartItem.delete({
-        where:{
+        where: {
             id: itemId
         }
     })
 
 
-     response.status(200).send({ message: "remove item from cart successfully", status: "success" });
+    response.status(200).send({ message: "remove item from cart successfully", status: "success" });
 })
 
 
-cartRouter.post('/', async(request, response)=>{
+cartRouter.post('/', async (request, response) => {
     const { productID, amount } = request.body;
 
-    
+
     const token = getTokenFrom(request.get('authorization'))
     //let decodedToken: DecodedToken|null = null;
 
     let userID = undefined
-    if(token!== null)
-    {
-         //decodedToken = jwt.verify(token, process.env.SECRET as string) 
-         userID = userIdFromJWT(token)
+    if (token !== null) {
+        //decodedToken = jwt.verify(token, process.env.SECRET as string) 
+        userID = userIdFromJWT(token)
     }
-   
-    if(userID === undefined)
-    {
-        response.status(400).send({message: "Error cannot find any user", status: "error"})
+
+    if (userID === undefined) {
+        response.status(400).send({ message: "Error cannot find any user", status: "error" })
         return
     }
-    
+
     const user = await prisma.customer.findUnique({
-        where:{
+        where: {
             id: userID
         },
-        include:{
+        include: {
             cart: true
         }
     })
 
     const alreadyItemObject = await prisma.cartItem.findFirst({
-        where:{
+        where: {
             productId: productID
         }
     })
 
-    if(!alreadyItemObject)
-    {
+    if (!alreadyItemObject) {
         const cartItemObject = await prisma.cartItem.create({
             data: {
                 productId: productID,
@@ -190,34 +181,36 @@ cartRouter.post('/', async(request, response)=>{
                     }
                 }
             }
-        }) 
-    }
-    else
-    {
-        const updateItem = await prisma.cartItem.update({
-            where:{
-                id: alreadyItemObject?.id
-            },
-            data:{
-                amount: alreadyItemObject.amount+ parseInt(amount)
+        })
+
+
+        const product = await prisma.product.findUnique({
+            where: {
+                id: productID
             }
         })
+
+        response.status(200).send({ itemId: cartItemObject.id, product: product, message: "add item to cart successfully", status: "success" });
+    }
+    else {
+        const updateItem = await prisma.cartItem.update({
+            where: {
+                id: alreadyItemObject?.id
+            },
+            data: {
+                amount: alreadyItemObject.amount + parseInt(amount)
+            }
+        })
+
+        const product = await prisma.product.findUnique({
+            where: {
+                id: productID
+            }
+        })
+        response.status(200).send({ itemId: alreadyItemObject.id, product: product, message: "add item to cart successfully", status: "success" });
     }
 
-    //console.log(user)
 
-    
-
-
-    const product = await prisma.product.findUnique({
-        where:{
-            id: productID
-        }
-    })
-
-    //console.log("add item to cart successfull")
-
-    response.status(200).send({product: product, message: "add item to cart successfully", status: "success" });
 })
 
 
